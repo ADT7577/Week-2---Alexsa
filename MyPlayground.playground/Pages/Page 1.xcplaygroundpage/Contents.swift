6205334e45285a7460437f47341240f8e09fca53
adt7577 <adt7577@nyu.edu>
2023-02-06 23:39:43 -0500

    Background Color

--- MyPlayground.playground/Pages/Page 1.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Page 1.xcplaygroundpage/Contents.swift
@@ -1,21 +1,37 @@
 import SwiftUI
 import PlaygroundSupport
 
+let wide = 712.0
+let high = 712.0
+
+
+
 struct ContentView: View {
-  var body: some View {
-    VStack(spacing: 0) {
-      Color.purple
-
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color(white:1))
+                .frame(width: wide, height: high)
+            
+        
+            
+            
+            VStack(spacing: 15) {
+                Color.purple
+                Color.pink
+                Color.orange
+                
+                
+            }
+            .mask(
+                Image(systemName: "brain.head.profile")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            )
+            .frame(width: 256, height: 256)
+        }
     }
-    .mask(
-      Image(systemName: "brain.head.profile")
-        .resizable()
-        .aspectRatio(contentMode: .fit)
-    )
-    .frame(width: 256, height: 256)
-  }
 }
-
 PlaygroundPage.current.setLiveView(
   ContentView()
     .frame(width: 375, height: 812)
